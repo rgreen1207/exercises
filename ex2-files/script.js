1a05c8f1115cdd6952f4af295f14edd24347654c
Ryan <rgreen@csumb.edu>
2017-12-11 21:55:03 -0800

    added button for dropdown menu

---
+++ ex2-files/script.js
@@ -0,0 +1,6 @@
+﻿function showNavMenu() {
+    var options = ["Home", "About", "Gallery", "Events", "News", "Contact"]
+    for (var i = 0; i < options.lenght; i++) {
+        document.write('<option value="' + options[i] + '">' + options[i] + '</options>');
+    }
+}
