45a5d96df4de6be0741cb868aa4d36078fd81768
Ryan <rgreen@csumb.edu>
2017-12-26 14:47:47 -0800

    Adding the dropdown button, working on getting elements to align properly before adding the javascript to hide and show

--- ex2-files/script.js
+++ ex2-files/script.js
@@ -1,6 +1,3 @@
-﻿function showNavMenu() {
-    var options = ["Home", "About", "Gallery", "Events", "News", "Contact"]
-    for (var i = 0; i < options.lenght; i++) {
-        document.write('<option value="' + options[i] + '">' + options[i] + '</options>');
-    }
+﻿function showhideNavMenu() {
+    var menu = document.getElementById("drop_menu");
 }
