f9734fccd1182b7d39af3e7e5841eb9c57e89419
Ryan <rgreen@csumb.edu>
2017-12-30 20:57:24 -0800

    added drop down, need to re-align

--- ex2-files/script.js
+++ ex2-files/script.js
@@ -1,3 +1,13 @@
 ﻿function showhideNavMenu() {
-    var menu = document.getElementById("drop_menu");
+    document.getElementById("drop_menu").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+    if (!event.target.matches('.menu_drop_button_holder') && !event.target.matches('.menu_drop_button')) {
+        document.getElementById("drop_menu").classList.remove('show');
+    }
+}
+
+window.screen.width = function (event) {
+    document.getElementById("drop_menu").classList.remove('show');
 }
