eda0697bd6ec63adac6eee799532f5726223322d
Tomer Almog <talmog@spokeo.com>
2018-01-02 17:57:57 -0800

    review

--- ex2-files/script.js
+++ ex2-files/script.js
@@ -1,9 +1,19 @@
-﻿function showhideNavMenu() {
-    document.getElementById("drop_menu").classList.toggle("show");
-}
+﻿(function () {
 
-window.onclick = function (event) {
+  var s = {
+    menu: document.getElementById("drop_menu"),
+    showClassName: 'show'
+  };
+
+
+  var menu = document.getElementById("drop_menu");
+  document.getElementById('drop-arrow').addEventListener('click', function (e) {
+    menu.classList.toggle(s.showClassName);
+  });
+
+  window.onclick = function (event) {
     if (!event.target.matches('.menu_drop_button_holder') && !event.target.matches('.menu_drop_button')) {
-        document.getElementById("drop_menu").classList.remove('show');
+      menu.classList.remove(s.showClassName);
     }
-}
+  }
+})();
