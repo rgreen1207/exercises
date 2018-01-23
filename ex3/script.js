ba86de24306c3d9baf9b16f7ae7bda90e47fb9f0
Ryan <rgreen@csumb.edu>
2018-01-23 00:41:05 -0800

    starting to include logic for image slider

--- ex3/script.js
+++ ex3/script.js
@@ -16,4 +16,41 @@
       menu.classList.remove(s.showClassName);
     }
   }
+
+  var images = {
+      "slider_images": [{
+          "img": "./images/gti_frontend.jpg",
+          "title": "GTI Frontend"
+      },
+      {
+          "img": "./images/roccos",
+          "title": "Roccos Rollout"
+      },
+      {
+          "img": "./images/bp_ach.jpg",
+          "title": "Brandon GTI on ACH"
+      },
+      {
+          "img": "./images/bp_interior.jpg",
+          "title": "Brandon Driver View"
+      },
+      {
+          "img": "./images/cb_night.jpg",
+          "title": "Christian Night Shot"
+      },
+      {
+          "img": "./images/highway.jpg",
+          "title": "Christian Highway"
+      },
+      {
+          "img": "./images/paramount_ranch.jpg",
+          "title": "SoCal Mk7 at Paramount Ranch"
+      }]
+  };
+
+  function rotate_images() {
+      main_img = document.getElementById("main_image");
+      
+  };
+
 })();
