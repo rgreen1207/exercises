ea5c4687b2fbe851734522b18c5b4fd4f004b572
Ryan <rgreen@csumb.edu>
2018-02-06 23:51:27 -0800

    clicking to scroll through images almost works as intended

--- ex3/script.js
+++ ex3/script.js
@@ -54,8 +54,17 @@
     var img_holder_3 = document.getElementById("img_3");
     var img_holder_4 = document.getElementById("img_4");
     var img_array = [img_holder_0, img_holder_1, center_img, img_holder_3, img_holder_4];
-    var main_img = document.getElementById("main_image")
+    var main_img = document.getElementById("main_image");
     var counter = 0;
+
+    var slideTimer = setInterval(function () {
+        if (counter > images.slider_images.length - 1) {
+            counter = 0;
+        }
+        slide_images(counter);
+        counter++;
+    }, 5000);
+
     function load_images() {
         count = 0
         img_array.forEach(function (item) {
@@ -63,26 +72,42 @@
             count++;
         });
         main_img.style.backgroundImage = "url(" + center_img.src + ")";
-        setInterval(function(){
-            slide_images(counter);
-            counter++;
-            if( counter > images.slider_images.length - 1) {
-                counter = 0;
-            }
-        },5000);
+        slideTimer
     };
     load_images();
 
     function slide_images(counter) {
         img_array.forEach(function (item) {
+            if (counter > (images.slider_images.length - 1)) {
+                counter = -1 + (counter - (images.slider_images.length - 1));
+            }
+            else if (counter < 0) {
+                counter = images.slider_images.length + counter;
+            };
             item.src = images.slider_images[counter].img;
             counter++;
-            if (counter > (images.slider_images.length - 1)) {
-                counter = 0;
-            };
         });
         main_img.style.backgroundImage = "url(" + center_img.src + ")";
     };
 
-    slide_images(counter);
+    img_holder_0.addEventListener("click", function () {
+        slide_images(counter = counter - 2);
+        clearInterval(slideTimer);
+    });
+
+    img_holder_1.addEventListener("click", function () {
+        slide_images(counter--);
+        clearInterval(slideTimer);
+    });
+
+    img_holder_3.addEventListener("click", function () {
+        slide_images(counter++);
+        clearInterval(slideTimer);
+    });
+
+    img_holder_4.addEventListener("click", function () {
+        slide_images(counter = counter + 2);
+        clearInterval(slideTimer);
+    });
+
 })();
