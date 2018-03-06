4eeb31ff423f3d705a4886fb50d05bfcb5411229
Ryan <rgreen@csumb.edu>
2018-03-06 01:52:26 -0800

    JS is cleaned up and the onclick for the images in now tied to their css

--- ex3/script.js
+++ ex3/script.js
@@ -5,7 +5,6 @@
         showClassName: 'show'
     };
 
-
     var menu = document.getElementById("drop_menu");
     document.getElementById('drop-arrow').addEventListener('click', function (e) {
         menu.classList.toggle(s.showClassName);
@@ -14,22 +13,6 @@
     window.onclick = function (event) {
         if (!event.target.matches('.menu_drop_button_holder') && !event.target.matches('.menu_drop_button')) {
             menu.classList.remove(s.showClassName);
-        }
-        if (event.target.matches('.side_img')) {
-            console.log(event.target.id);
-            if (event.target.id == "img_0") {
-                slide_images(counter = counter - 2);
-            }
-            else if (event.target.id == "img_1") {
-                slide_images(counter--);
-            }
-            else if (event.target.id == "img_3") {
-                slide_images(counter++);
-            }
-            else if (event.target.id == "img_4") {
-                slide_images(counter = counter + 2);
-            }
-            counter = check_array_index(counter);
         }
     }
 
@@ -64,47 +47,49 @@
         }]
     };
 
-    var img_holder_0 = document.getElementById("img_0");
-    var img_holder_1 = document.getElementById("img_1");
-    var center_img = document.getElementById("center_img");
-    var img_holder_3 = document.getElementById("img_3");
-    var img_holder_4 = document.getElementById("img_4");
-    var img_array = [img_holder_0, img_holder_1, center_img, img_holder_3, img_holder_4];
+    var img_array = document.getElementsByClassName("img_preview");
     var main_img = document.getElementById("main_image");
     var strtstpbtn = document.getElementById("play_pause_btn");
     var counter = 0;
     var play = true;
+    var img_holders = Array.from(document.getElementsByClassName("side_img"));
 
-    var slideTimer = setInterval(function () {
+    var slideTimer = setInterval(slideTimerFunction, 5000);
+
+    function slideTimerFunction() {
         if (counter > images.slider_images.length - 1) {
             counter = 0;
         }
         slide_images(counter);
         counter++;
-    }, 5000);
+    }
 
     function load_images() {
         count = 0
-        img_array.forEach(function (item) {
-            item.src = images.slider_images[count].img;
-            count++;
-        });
-        main_img.style.backgroundImage = "url(" + center_img.src + ")";
-        slideTimer
+        for (var i = 0; i < img_array.length; i++) {
+            count = place_images(i, count);
+        }
+        main_img.style.backgroundImage = "url(" + img_array[2].src + ")";
+        slideTimer;
     };
     load_images();
 
     function slide_images(img_pos) {
-        img_array.forEach(function (item) {
+        for (var i = 0; i < img_array.length; i++) {
             img_pos = check_array_index(img_pos)
-            item.src = images.slider_images[img_pos].img;
-            img_pos++;
-        });
-        main_img.style.backgroundImage = "url(" + center_img.src + ")";
+            img_pos = place_images(i, img_pos);
+        }
+        main_img.style.backgroundImage = "url(" + img_array[2].src + ")";
+    };
+
+    function place_images(index, img_index) {
+        img_array[index].src = images.slider_images[img_index].img;
+        img_index++;
+        return img_index;
     };
 
     function check_array_index(index) {
-        if ((index == images.slider_images.length) || (index < 0)) {
+        if ((index >= images.slider_images.length) || (index < 0)) {
             index = index % images.slider_images.length;
         }
         return index;
@@ -120,13 +105,22 @@
             play = true;
             slide_images(counter++);
             counter = check_array_index(counter);
-            slideTimer = setInterval(function () {
-                if (counter > images.slider_images.length - 1) {
-                    counter = 0;
-                }
-                slide_images(counter);
-                counter++;
-            }, 5000);
+            slideTimer = setInterval(slideTimerFunction, 5000);
         }
+    });
+
+    img_holders.forEach(function (element) {
+        element.addEventListener("click", function () {
+            clearInterval(slideTimer);
+            var elm_ind = img_holders.indexOf(element);
+            if (elm_ind == 0 || elm_ind == 1) {
+                slide_images(counter = counter - elm_ind);
+            }
+            else {
+                slide_images(counter = counter + (elm_ind - 1));
+            }
+            counter = check_array_index(counter);
+            slideTimer = setInterval(slideTimerFunction, 5000);
+        });
     });
 })();
