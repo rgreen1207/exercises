ba41451b5bb84f903086d6f3868b3f3a1d165652
Ryan Green <rgreen@spokeo.com>
2018-05-09 20:44:22 -0700

    correcting file call

--- ex4/webpack.config.js
+++ ex4/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: 'script.js',
+  entry: './script.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
