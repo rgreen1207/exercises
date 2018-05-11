2fe736cec1f437622a04d43f0eb04afe9565903f
Ryan Green <rgreen@spokeo.com>
2018-05-10 22:38:22 -0700

    adding webpack for images and css

--- ex4/webpack.config.js
+++ ex4/webpack.config.js
@@ -3,7 +3,24 @@ const path = require('path');
 module.exports = {
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {}
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      }
     ]
   },
   entry: './src/index.js',
