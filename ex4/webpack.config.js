18d4860989d7d3e204f6522fbb39492a35617d43
Ryan Green <rgreen@spokeo.com>
2018-05-10 22:46:31 -0700

    cleaning webpack file

--- ex4/webpack.config.js
+++ ex4/webpack.config.js
@@ -19,7 +19,10 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
       }
     ]
   },
