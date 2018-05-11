4b98fabc0d85f2525e22d27ed1de9d8aaee8a1a4
Ryan Green <rgreen@spokeo.com>
2018-05-10 22:21:31 -0700

    relocating files to proper folders

--- ex4/src/index.js
+++ ex4/src/index.js
@@ -18,39 +18,33 @@
     if (!event.target.matches('.menu_drop_button_holder') && !event.target.matches('.menu_drop_button')) {
       s.menu.classList.remove(s.showClassName);
     }
-  }
+  };
 
   var images = {
     "sliderImages": [{
       "img": "./images/highway.jpg",
       "title": "Christian Highway"
-    },
-      {
-        "img": "./images/paramount_ranch.jpg",
-        "title": "SoCal Mk7 at Paramount Ranch"
-      },
-      {
-        "img": "./images/gti_frontend.jpg",
-        "title": "GTI Frontend"
-      },
-      {
-        "img": "./images/roccos.jpg",
-        "title": "Roccos Rollout"
-      },
-      {
-        "img": "./images/bp_ach.jpg",
-        "title": "Brandon GTI on ACH"
-      },
-      {
-        "img": "./images/cb_night.jpg",
-        "title": "Christian Night Shot"
-      },
-      {
-        "img": "./images/bp_interior.jpg",
-        "title": "Brandon Driver View"
-      }]
+    }, {
+      "img": "./images/paramount_ranch.jpg",
+      "title": "SoCal Mk7 at Paramount Ranch"
+    }, {
+      "img": "./images/gti_frontend.jpg",
+      "title": "GTI Frontend"
+    }, {
+      "img": "./images/roccos.jpg",
+      "title": "Roccos Rollout"
+    }, {
+      "img": "./images/bp_ach.jpg",
+      "title": "Brandon GTI on ACH"
+    }, {
+      "img": "./images/cb_night.jpg",
+      "title": "Christian Night Shot"
+    }, {
+      "img": "./images/bp_interior.jpg",
+      "title": "Brandon Driver View"
+    }]
   };
-  
+
   var counter = 0;
   var play = true;
   var slideTimer = setInterval(slideTimerFunction, 5000);
@@ -64,7 +58,7 @@
   }
 
   function load_images() {
-    count = 0
+    count = 0;
     for (var i = 0; i < s.imgArray.length; i++) {
       count = place_images(i, count);
     }
@@ -75,7 +69,7 @@
 
   function slide_images(imgPosition) {
     for (var i = 0; i < s.imgArray.length; i++) {
-      imgPosition = check_array_index(imgPosition)
+      imgPosition = check_array_index(imgPosition);
       imgPosition = place_images(i, imgPosition);
     }
     s.mainImg.style.backgroundImage = "url(" + s.imgArray[2].src + ")";
@@ -90,8 +84,7 @@
   function check_array_index(index) {
     if (index >= images.sliderImages.length) {
       index = index % images.sliderImages.length;
-    }
-    else if (index < 0) {
+    } else if (index < 0) {
       index = images.sliderImages.length - Math.abs(index);
     }
     return index;
@@ -99,11 +92,10 @@
 
   s.startStopBtn.addEventListener("click", function () {
     console.log(play);
-    if(play == true){
+    if (play == true) {
       clearInterval(slideTimer);
       play = false;
-    }
-    else {
+    } else {
       play = true;
       slide_images(counter++);
       counter = check_array_index(counter);
@@ -117,8 +109,7 @@
       var elementIndex = s.imgHolders.indexOf(element);
       if (elementIndex == 0 || elementIndex == 1) {
         slide_images(counter = counter + (elementIndex - 2));
-      }
-      else {
+      } else {
         slide_images(counter = counter + (elementIndex - 1));
       }
       counter = check_array_index(counter);
