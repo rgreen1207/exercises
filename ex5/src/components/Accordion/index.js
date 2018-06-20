7fc74daae56a376bbe0cc9535e7726e475e5922f
Ryan Green <rgreen@spokeo.com>
2018-06-19 17:28:33 -0700

    added 2nd lang

--- ex5/src/components/Accordion/index.js
+++ ex5/src/components/Accordion/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Section from '../Section/index.js';
 
-const sectionList = [
+const sectionJapanese = [
   {title: "One", text: "一 (ichi)"},
   {title: "Two", text: "二 (ni)"},
   {title: "Three", text: "三 (san)"},
@@ -14,9 +14,29 @@ const sectionList = [
   {title: "Ten", text: "十 (jū)"}
 ];
 
+const sectionChinese = [
+  {title: "One", text: "一 (yī)"},
+  {title: "Two", text: "二 (èr)"},
+  {title: "Three", text: "三 (sān)"},
+  {title: "Four", text: "四 (sì)"},
+  {title: "Five", text: "五 (wǔ)"},
+  {title: "Six", text: "六 (liù)"},
+  {title: "Seven", text: "七 (qī)"},
+  {title: "Eight", text: "八 (bā)"},
+  {title: "Nine", text: "九 (jiǔ)"},
+  {title: "Ten", text: "十 (shí)"}
+];
+
+const buttonList = [
+  {text: "Japanese", fnct: sectionJapanese},
+  {text: "Chinese", fnct: sectionChinese}
+]
+
 class Accordion extends React.Component {
   state = {
-    openSection: false
+    openSection: false,
+    lang: sectionJapanese,
+    language: "Japanese"
   };
 
   sectionOpenCallback = (sectionId) => {
@@ -25,7 +45,7 @@ class Accordion extends React.Component {
     });
   };
 
-  sectionJSX = () => {
+  sectionJSX = (sectionList) => {
     return sectionList.map((section, i) => {
       return <Section key={i}
                       title={section.title}
@@ -35,13 +55,25 @@ class Accordion extends React.Component {
         {section.text}
       </Section>
     });
-  }
+  };
+
+  langButtons = buttonList.map((btn, i)=>{
+    return <button key={i}
+                   onClick={() => this.setState({
+                     lang: btn.fnct,
+                     language: btn.text
+                   })}>
+      {btn.text}
+    </button>
+  });
 
   render() {
     return (
       <div>
         <div>{this.props.title}</div>
-        {this.sectionJSX()}
+        {this.langButtons}
+        <div>Language: {this.state.language}</div>
+        {this.sectionJSX(this.state.lang)}
       </div>
     );
   }
